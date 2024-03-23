5f6d6a3344ed9763ab2baa54f48a938e115b15b6
Eman__Iqbal <160255293+programmerEI@users.noreply.github.com>
2024-03-23 15:39:49 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+var a = 5;
+var b = 2;
+var c;
+//6     6  + 12  +  13 + 14   + 21+ 21  + 22
+c = ++a + a++ + --b + b-- + a + b++ + b;
+console.log(c);
+// combining operators
+var result = 3 + 4 * 5; //23
+console.log(result);
+//Addition
+var num1 = 2;
+var num2 = 3;
+var Addition = num1 + num2;
+console.log(Addition);
+//  //subtraction
+var num3 = 7;
+var num4 = 2;
+var subtraction = num3 - num4;
+console.log(subtraction);
+//  //multiplication
+var num5 = 7;
+var num6 = 2;
+var muliplication = num5 * num6;
+console.log(muliplication);
+//  //Division
+var num7 = 7;
+var num8 = 2;
+var division = num7 / num8;
+console.log(division);
+//exponentiation
+var layer = 5;
+var apple = 5;
+var power = Math.pow(layer, 2);
+console.log(power);
+// modules
+var apples = 5;
+var bags = 2;
+var reminder = apples % bags;
+console.log(reminder);
---
+++ main.ts
@@ -0,0 +1,48 @@
+let a:number = 5; 
+let b: number = 2;
+let c: number;          
+//6     6  + 12  +  13 + 14   + 21+ 21  + 22
+ c = ++a + a++ + --b + b--  + a + b++ + b ;
+ console.log(c);
+
+
+ // combining operators
+ let result = 3 + 4 * 5; //23
+ console.log(result);
+
+ //Addition
+ let num1:number = 2 ;
+ let num2:number = 3;
+ let Addition = num1 + num2;
+ console.log(Addition);
+
+//  //subtraction
+ let num3:number = 7;
+let num4:number =2;
+let subtraction = num3 - num4;
+  console.log(subtraction);
+
+//  //multiplication
+ let num5:number = 7;
+ let num6:number =2;
+let muliplication = num5 * num6 ;
+  console.log(muliplication);
+
+//  //Division
+  let num7:number = 7;
+  let num8:number =2;
+let division = num7 / num8 ;
+  console.log( division);
+
+ //exponentiation
+ let layer = 5 
+ let apple = 5
+ let power = layer **2;
+ console.log(power);
+
+// modules
+ let apples =5 
+ let bags = 2
+ let reminder = apples % bags;
+ console.log(reminder);
+
